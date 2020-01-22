290913de3cfc5c01d7d6d2f5f03ab664cc096423
Mateusz <mateuszcwojdzinski@outlook.com>
2020-01-22 10:29:11 +0100

    Add files into the project

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" type="image/png" href="images/favicon.png"/>
+    <link href="styles/main.css" rel="stylesheet" type="text/css">
+    <title>Tree Store</title>
+</head>
+<body>
+    <header class="header">
+        <div class="logo">
+            <img src="images/logo.png" class="logo__picture" alt="Our brand logo">
+            <h1 class="logo__text">Tree-Store</h1>
+        </div>
+        <div class="hamburger">
+            <img src="images/menu.svg" alt="hamburger logo">
+        </div>
+    </header>
+    <nav class="navigation">
+
+    </nav>
+    <main class="main">
+    
+    </main>
+    <section class="section">
+        <article class="article"></article>
+        <article class="article"></article>
+        <article class="article"></article>
+    </section>
+    <footer class="footer">
+</body>
+</html>
