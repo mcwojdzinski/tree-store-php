59d5219ae38e98073b67b85748574f5c71d22f09
Mateusz <mateuszcwojdzinski@outlook.com>
2020-02-14 09:34:02 +0100

    Add login.php, about.php and styling to this page

---
+++ subpages/about.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" type="image/png" href="../images/favicon.png"/>
+    <link href="../styles/main.css" rel="stylesheet" type="text/css">
+    <title>Tree Store</title>
+</head>
+<body>
+    <header class="header">
+        <div class="logo">
+            <img src="../images/logo.png" class="logo__picture" alt="Our brand logo">
+            <h1 class="logo__text">Tree-Store</h1>
+        </div>
+        <div class="hamburger">
+            <img src="../images/hamburger.png" class="hamburger__icon" alt="hamburger-menu">
+        </div>
+    </header>
+    <nav class="nav">
+    </nav>
+    <main class="content">
+        <h1 class="content__header">About us</h1>
+        <p class="content__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, dolore porro expedita veniam tempora esse? Esse inventore laboriosam quisquam nulla dolore minima, rerum officiis fugit cum praesentium odio repellendus voluptas!Vero eius vitae, perspiciatis dolor doloremque ad aliquam necessitatibus excepturi quae suscipit eos temporibus earum est placeat molestias inventore blanditiis et? Tenetur facilis, omnis ullam deserunt libero tempora similique nisi!</p>
+        <h3 class="content__header">Our history</h3>
+        <div class="history">
+            <ul class="history__list">
+                <li>2009 - Establishing our company</li>
+                <li>2013 - laureate in "The best tree store"</li> 
+            </ul>
+        </div>
+    </main>
+    <footer class="footer"></footer>
+</body>
+</html>
